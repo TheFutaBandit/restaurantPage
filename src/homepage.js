4c5b5485f7836c34c60dc87d89639c6458915928
TheFutaBandit <prameshanand2021@gmail.com>
2024-09-25 01:49:59 +0530

    homepage + home button functionality added

---
+++ src/homepage.js
@@ -0,0 +1,36 @@
+import wings_image from "./wings.jpg"
+
+export const homeScreenBootup = (() => {
+
+    const content = document.querySelector("#content");
+
+    const main_title = document.createElement("h1");
+    main_title.classList.add("main_title")
+    main_title.textContent = "Wingstop Bar"
+    content.appendChild(main_title);
+
+    const menu = document.createElement("div");
+    menu.classList.add("menu");
+    content.appendChild(menu);
+
+    const menu_items = document.createElement("div");
+    menu_items.classList.add("menu-items");
+    menu_items.textContent = "I love Chicken Wings. Moreover, I love the absolute flavour variety. Not to mention when you dip it in sauce oh my fucking god the taste. too good.";
+    menu.appendChild(menu_items);
+
+    const menu_timings = document.createElement("div");
+    menu_timings.classList.add("menu-timings");
+    menu_timings.textContent = "24/7";
+    menu.appendChild(menu_timings);
+
+    const menu_location = document.createElement("div");
+    menu_location.classList.add("menu-location");
+    menu_location.textContent = "123-Location, Hello Boulevard, New Park";
+    menu.appendChild(menu_location);
+
+    const img_wings = document.createElement("img");
+    img_wings.classList.add("wings");
+    img_wings.src = wings_image;
+    menu.appendChild(img_wings);
+
+});
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("hi")
+import wings_image from "./wings.jpg"
+
+import { homeScreenBootup } from "./homepage"
+
+
+const webpageLoader = (() => {
+    const home = document.querySelector(".home");
+    const menu = document.querySelector(".menu");
+    const about = document.querySelector(".about");
+
+    function clearPage() {
+        const content = document.querySelector("#content");
+        content.textContent = "";
+    }
+
+    home.addEventListener("click", () => {
+        clearPage();
+        homeScreenBootup();
+    })
+
+    menu.addEventListener("click", () => {
+        clearPage();
+        homeScreenBootup();
+    })
+
+    about.addEventListener("click", () => {
+        clearPage();
+        homeScreenBootup();
+    })
+})();
